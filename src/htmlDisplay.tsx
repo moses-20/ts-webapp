20327eabc3afdbe6edac6314bd0ab20b022f2274
moozeire <moozeire@gmail.com>
2021-08-10 19:34:31 +0000

    dev: complete ts-webapp part 1

--- src/htmlDisplay.tsx
+++ src/htmlDisplay.tsx
@@ -1,20 +1,53 @@
-import {Product, Order} from './data/entities'
-import { createElement } from './tools/jsxFactory'
+import { createElement } from "./tools/jsxFactory";
+import { Product, Order } from "./data/entities";
+import { AbstractDataSource } from "./data/abtractDataSource";
+import { ProductList } from "./productList";
 
-export class HTMLDisplay {
-    props: {
-        products: Product[],
-        order: Order
-    }
+export class HtmlDisplay {
+  private containerElem: HTMLElement;
+  private selectedCategory: string;
 
-    getContent(): HTMLElement {
-        return <h3 className='bg-secondary text-center text-white p-2'>
-            {this.getElementText()}
-        </h3>
-    }
+  constructor() {
+    this.containerElem = document.createElement("div");
+  }
 
-    getElementText() {
-        return `${this.props.products.length} Products, ` +
-        `Order total: $${this.props.order.total}`
-    }
+  props: {
+    dataSource: AbstractDataSource;
+  };
+
+  async getContent(): Promise<HTMLElement> {
+    await this.updateContent();
+    return this.containerElem;
+  }
+
+  async updateContent() {
+    let products = await this.props.dataSource.getProducts(
+      "id",
+      this.selectedCategory
+    );
+    let categories = await this.props.dataSource.getCategories();
+    this.containerElem.innerHTML = "";
+    let content = (
+      <div>
+        <ProductList
+          products={products}
+          categories={categories}
+          selectedCategory={this.selectedCategory}
+          addToOrderCallback={this.addToOrder}
+          filterCallback={this.selectCategory}
+        />
+      </div>
+    );
+    this.containerElem.appendChild(content);
+  }
+
+  addToOrder = (product: Product, quantity: number) => {
+    this.props.dataSource.order.addProduct(product, quantity);
+    this.updateContent();
+  };
+
+  selectCategory = (selected: string) => {
+    this.selectedCategory = selected === "All" ? undefined : selected;
+    this.updateContent();
+  };
 }
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,14 @@
 import { LocalDataSource } from "./data/localDataSource";
-import { HTMLDisplay } from "./htmlDisplay";
+import { HtmlDisplay } from "./htmlDisplay";
 import "bootstrap/dist/css/bootstrap.css";
 
 let ds = new LocalDataSource();
 
-async function displayData(): Promise<HTMLElement> {
-  let display = new HTMLDisplay();
+function displayData(): Promise<HTMLElement> {
+  let display = new HtmlDisplay();
   display.props = {
-    products: await ds.getProducts("name"),
-    order: ds.order,
+    dataSource: ds,
   };
-
   return display.getContent();
 }
 
